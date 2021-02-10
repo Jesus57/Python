88fa373146b2a13ddde38a250269b8621857b544
DESKTOP-DOOLO0V\Jesu\github <jesusrosadopereira@gmail.com>
2021-02-10 14:38:17 +0100

    Escribir un ciclo definido que salude por pantalla a sus cinco mejores amigos/as

---
+++ Tanda1/02.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 01b
+b) Escribir un ciclo definido que salude por pantalla a sus cinco mejores
+amigos/as.
+Hola Luis
+Hola José Antonio
+Hola Miguel
+Hola Paco
+Hola Jaime
+"""
+for amigo in ["Jose","Dani","Antonio","Alejandro","Laimma"]:
+    print("Hey", amigo)
