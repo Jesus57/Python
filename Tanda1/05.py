1669d7a50710ca17ec7c4dbb7b0ff6d2152ef804
DESKTOP-DOOLO0V\Jesu\github <jesusrosadopereira@gmail.com>
2021-02-10 14:39:49 +0100

    Fº a Cº

---
+++ Tanda1/05.py
@@ -0,0 +1,7 @@
+"""
+Ejercicio 03
+Escribir un programa que convierta un valor dado en grados Fahrenheit a grados
+Celsius. Recordar que la fórmula para la conversión es: F = 9/5 * C + 32.
+"""
+faren = float(input("Digame los grados farengeis"))
+print("Hace ", (faren-32)*5/9," grados celsius")
